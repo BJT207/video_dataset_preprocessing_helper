94c2c6b2108498884156371998f7e633964fac3b
BJT207 <56009694+BJT207@users.noreply.github.com>
2025-11-21 14:35:26 -0500

    removed some unnecessary dependencies for optimization

--- ImageSelector.py
+++ ImageSelector.py
@@ -1,6 +1,5 @@
 import cv2
 from tqdm import tqdm
-from graphlib import draw_graph
 
 
 class ImageSelector:
@@ -30,25 +29,7 @@ class ImageSelector:
                 distribution[i] += 1
                 accumulated_error -= 1.0
 
-        return distribution, ideal_per_group
-
-    def generate_deleted_images_graph(self, selected_images):
-        bins = 100
-        step = len(self.images) // bins
-        percentages = []
-
-        for i in range(bins):
-            start_idx = i * step
-            end_idx = (i + 1) * step if i != bins - 1 else len(self.images)
-            current_bin = self.images[start_idx:end_idx]
-            deleted_count = sum(1 for img in current_bin if img not in selected_images)
-            avg = deleted_count / len(current_bin)
-            percentages.append(avg * 100)
-
-        draw_graph(percentages, "Distribution of to-be-deleted images")
-
-    def generate_quality_graph(self):
-        draw_graph([quality for quality, _ in self.image_fm], "Distribution of image quality")
+        return distribution
 
     def filter_sharpest_images(self, target_count, group_count=None, scalar=1):
         if scalar is None:
@@ -57,18 +38,9 @@ class ImageSelector:
             group_count = target_count // (2 ** (scalar - 1))
             group_count = max(1, group_count)
 
-        split = len(self.images) / target_count
-        ratio = target_count / len(self.images)
-        formatted_ratio = "{:.1%}".format(ratio)
-        print(f"Requested {target_count} out of {len(self.images)} images ({formatted_ratio}, 1 in {split:.1f}).")
+        group_sizes = self.distribute_evenly(len(self.images), group_count)
 
-        group_sizes, ideal_total_images_per_group = self.distribute_evenly(len(self.images), group_count)
-        images_per_group_list, ideal_selected_images_per_group = self.distribute_evenly(target_count, group_count)
-
-        print(f"Selecting {target_count} image{'s' if target_count != 1 else ''} across {group_count} group{'s' if group_count != 1 else ''}, with total ~{ideal_total_images_per_group:.1f} image{'s' if ideal_total_images_per_group != 1 else ''} per group and selecting ~{ideal_selected_images_per_group:.1f} image{'s' if ideal_selected_images_per_group != 1 else ''} per group (scalar {scalar}).")
-        draw_graph([(i % 2) for i in range(group_count)], "Group layout", 100)
-
-        images_per_group_list, _ = self.distribute_evenly(target_count, group_count)
+        images_per_group_list = self.distribute_evenly(target_count, group_count)
 
         selected_images = []
         offset_index = 0
@@ -77,8 +49,5 @@ class ImageSelector:
             group = sorted(self.image_fm[offset_index:end_idx], reverse=True)
             selected_images.extend([img[1] for img in group[:images_per_group_list[idx]]])
             offset_index = end_idx
-
-        self.generate_deleted_images_graph(selected_images)
-        self.generate_quality_graph()
 
         return selected_images
